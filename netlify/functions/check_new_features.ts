eb964fe79d6cd3cdc74efd81ba3b9c85267f54d1
Jose Daniel <jose.gomez4@utp.edu.co>
2026-08-19 13:10:13 -0500

    arreglo

--- netlify/functions/check_new_features.ts
+++ netlify/functions/check_new_features.ts
@@ -5,12 +5,12 @@ export default async () => {
     const response = await fetch("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson");
 
     const data = await response.json();
-    const latest = data[0];
+    const latest = data.features[0];
 
     const stored_latest = await getFeature();
 
     if (latest.id !== stored_latest.feature_id){
-       updateFeature(latest) 
+       await updateFeature(latest) 
     }
 
     return new Response("ok");
