a64c40706a9a80a9466580482c0fbd3afea78213
Jose Daniel <jose.gomez4@utp.edu.co>
2026-08-19 01:11:05 -0500

    debug

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ import {getData, getLatest} from "../api/usgs_api";
 export default async function Home() {
   const latestData = await getLatest();
   return (
-      {
-        /* <div className="flex h-full w-full">
-      <main>
+    <div className="flex h-full w-full">
+      hola
+      {/* <main>
         <Map children = {latestData}/>
         <InfoBar children = {latestData}></InfoBar>
         <p className="text-lg font-space-gortesk absolute bottom-0 left-2">TerraTracker</p>
-      </main>
-    </div> */}
+      </main> */}
+    </div>
   );
 }
 
