bff3eb29ee5723a8b7f32688c15e4b8242d34cc6
Jose Daniel <jose.gomez4@utp.edu.co>
2026-08-19 01:40:25 -0500

    fin del debug

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,11 @@ export default async function Home() {
   const latestData = await getLatest();
   return (
     <div className="flex h-full w-full">
-      hola
-      {/* <main>
+      <main>
         <Map children = {latestData}/>
         <InfoBar children = {latestData}></InfoBar>
         <p className="text-lg font-space-gortesk absolute bottom-0 left-2">TerraTracker</p>
-      </main> */}
+      </main>
     </div>
   );
 }
